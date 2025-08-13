db239c34f2737910b58994e59127cc07c8bb8a5e
JuniorPrado <juniorcvnnn@gmail.com>
2025-08-13 11:15:12 -0300

    adjust functions and routes

--- backend/app/api/v1/routes/genieacs.py
+++ backend/app/api/v1/routes/genieacs.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter
-from app.services.genieacs.devices import get_device_by_mac, get_all_devices
+from app.services.genieacs.devices import Genie
 
 router = APIRouter(
     prefix="/genie",
+    tags=["GENIE"]
 )
 
-@router.get("/devices", tags=["Todos os dispositivos"])
+@router.get("/devices")
 async def get_all():
-    return await get_all_devices()
+    '''
+    Retorna informações de todos os dispositivos conectados ao GENIE
+    '''
+    return await Genie.get_all_devices()
 
-@router.get("/devices/{mac_address}", tags=["Dispositivo por MAC"])
+@router.get("/devices/{mac_address}")
 async def get_by_mac(mac_address: str):
-    return await get_device_by_mac(mac_address=mac_address)
+    '''
+    Retorna informações de um disposivo específico. Para isso informe o endereço MAC do dispositivo ao final da url.
+    '''
+    return await Genie.get_device_by_mac(mac_address=mac_address)
